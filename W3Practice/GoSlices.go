988726a1a98eaeb4a0fddcb8cc87f8edb842876d
Jose De La Luz <Jdelaluz01@gmail.com>
2023-12-06 20:50:29 -0600

    Go slice

---
+++ W3Practice/GoSlices.go
@@ -0,0 +1,73 @@
+package main
+
+import "fmt"
+
+/*
+	Slices are similar to arrays, but are more powerful and flexible.
+
+	Like arrays, slices are also used to store multiple values of the same type in a single variable.
+
+	However, unlike arrays, the length of a slice can grow and shrink as you see fit.
+
+	In Go, there are several ways to create a slice:
+
+	Using the []datatype{values} format
+	Create a slice from an array
+	Using the make() function
+*/
+
+// Create a Slice With []datatype{values}
+func SliceWDatatype() {
+	myslice1 := []int{}
+	fmt.Println(len(myslice1))
+	fmt.Println(cap(myslice1))
+	fmt.Println(myslice1)
+
+	myslice2 := []string{"Go", "Slices", "Are", "Powerful"}
+	fmt.Println(len(myslice2))
+	fmt.Println(cap(myslice2))
+	fmt.Println(myslice2)
+}
+
+// Create a Slice with a array
+func SliceWArray() {
+	/*
+		SYNTAX DIFFERENCE BETWEEN SLICE W/ ARRAY VS ARRAY
+		var myarray = [length]datatype{values} // An array
+		myslice := myarray[start:end] // A slice made from the array
+	*/
+	//first create array
+	arr1 := [6]int{10, 11, 12, 13, 14, 15}
+	//second pust start and end
+	myslice := arr1[0:len(arr1)]
+
+	fmt.Printf("myslice = %v\n", myslice)
+	fmt.Printf("length = %d\n", len(myslice))
+	fmt.Printf("capacity = %d\n", cap(myslice))
+}
+
+//Create a Slice With The make() Function
+
+func SliceWMakeFunc() {
+	/*
+		SYNTAX
+		slice_name := make([]type, length, capacity)
+
+		If the capacity parameter is not defined, it will be equal to length.
+	*/
+	myslice1 := make([]int, 5, 10)
+	fmt.Printf("myslice1 = %v\n", myslice1)
+	fmt.Printf("length = %d\n", len(myslice1))
+	fmt.Printf("capacity = %d\n", cap(myslice1))
+
+	// with omitted capacity
+	myslice2 := make([]int, 5)
+	fmt.Printf("myslice2 = %v\n", myslice2)
+	fmt.Printf("length = %d\n", len(myslice2))
+	fmt.Printf("capacity = %d\n", cap(myslice2))
+}
+func main() {
+	//SliceWDatatype()
+	//SliceWArray()
+	//SliceWMakeFunc()
+}
